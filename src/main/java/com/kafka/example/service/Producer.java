2bd2b5044af1bdc344a2ee2a448a234c1c38e2c2
Konstantin <konstantin@revimedia.com>
2023-02-13 02:15:03 +0200

    Fix silly error with removing last starter

--- src/main/java/com/kafka/example/service/Producer.java
+++ src/main/java/com/kafka/example/service/Producer.java
@@ -4,19 +4,24 @@ import com.kafka.example.events.SyncEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class Producer {
-   private final KafkaTemplate<String, SyncEvent> kafkaTemplate;
+    private final KafkaTemplate<String, SyncEvent> kafkaTemplate;
 
     public void sendSynEvent() {
         log.info("About to send sync event");
         log.info("Default topic = {}", kafkaTemplate.getDefaultTopic());
 //        kafkaTemplate.sendDefault(SyncEvent.createDefault());
-        kafkaTemplate.send( kafkaTemplate.getDefaultTopic(), SyncEvent.createDefault());
+        kafkaTemplate.send(kafkaTemplate.getDefaultTopic(), SyncEvent.createDefault())
+                .thenApply(SendResult::getRecordMetadata)
+                .thenAccept(metadata -> log.info("Metadata = {}", metadata));
         log.info("Sent sync event");
     }
 }
