0961e0a04ff0c0a0f7e95eb465fa9c7d54520f88
Konstantin <konstantin@revimedia.com>
2023-02-15 00:40:44 +0200

    Fix path to kafka scripts

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 # Start the ZooKeeper service
-${KAFKA_HOME}\bin\windows\zookeeper-server-start.sh config\zookeeper.properties
+%KAFKA_HOME%\bin\windows\zookeeper-server-start.bat %KAFKA_HOME%\config\zookeeper.properties
 
 # Start the Kafka broker service
-${KAFKA_HOME}\bin\windows\kafka-server-start.sh config\server.properties
+%KAFKA_HOME%\bin\windows\kafka-server-start.bat %KAFKA_HOME%\config\server.properties
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start the ZooKeeper service
-${KAFKA_HOME}/bin/zookeeper-server-start.sh config/zookeeper.properties
+${KAFKA_HOME}/bin/zookeeper-server-start.sh ${KAFKA_HOME}/config/zookeeper.properties
 
 # Start the Kafka broker service
-${KAFKA_HOME}/bin/kafka-server-start.sh config/server.properties
+${KAFKA_HOME}/bin/kafka-server-start.sh ${KAFKA_HOME}/config/server.properties
